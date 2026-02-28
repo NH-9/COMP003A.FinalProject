f648ed0020e34c597f4f4972823328ecb4c1c72f
NH <noahhulett@my.whccd.edu>
2026-02-27 18:47:11 -0800

    class fields declared

---
+++ COMP003A.FinalProject/Pokemon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Pokemon
+{
+	public string Species;
+	public string Nickname;
+	public string Type1;
+	public string Type2;
+	public string Item;
+
+	public string Ability;
+	public string Nature;
+	public string TeraType;
+	public string Offense;
+	public string Defense;
+
+	public string TeamRole;
+	public int HitPoints;
+	public int PhysAttack;
+	public int SpecAttack;
+	public int PhysDefense;
+
+	public int SpecDefense;
+	public int BaseStatTotal;
+	public int Speed;
+	public int Level;
+	public int DexNumber;
+
+	public bool Shiny;
+	public bool FullEvolved;
+	public bool Legendary;
+	public bool Mythical;
+	public bool CanMegaEvolve;
+		
+	public Pokemon()
+	{
+	}
+}
