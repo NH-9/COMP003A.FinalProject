2cb5c49c7dc3b2e3f72c3d8482d6d4cd107c090c
NH <noahhulett@my.whccd.edu>
2026-02-27 19:16:34 -0800

    menu prompt and input validation

--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Pokemon Information Storage System");
+            Console.WriteLine("----------------------------------");
+
+            int choice = 0;
+
+            while (choice != 5)
+            {
+                Console.WriteLine("1. Add New Pokemon \n2. View All Records \n3. Search Records \n4. Display Summary Statistics \n5. Exit\n");
+                choice = ValidateInput();
+                Console.WriteLine(choice);
+            }
+
+            static int ValidateInput()
+            {
+                int choice = 0;
+                bool valid = false;
+                
+                while (!valid)
+                {
+                    Console.Write("Enter Choice: ");
+
+                    try
+                    {
+                        choice = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid Numeric Input\n");
+                        continue;
+                    }
+
+                    if (choice >= 1 && choice <= 5)
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number Out Of Range\n");
+                    }
+                }
+
+                return choice;
+            }
+
         }
     }
 }
